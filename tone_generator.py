22f48933fee7748d7796a8e7d5c11a7527fdd86e
skuhn002 <kuhnsamuel2@gmail.com>
2021-07-15 15:49:40 -0400

    Turn Code Into Function
    Create a function for running this code so I can parameterize the delay. Added two parameters: rand_delay and my_delay. rand_delay is for controlling whether or not to use a random delay. my_delay is for controlling a custom set delay time (in seconds)

--- tone_generator.py
+++ tone_generator.py
@@ -2,9 +2,19 @@ from random import randint
 import playsound
 from time import sleep
 
-delay = randint(2, 9) + 3
-print(delay)
 
-sleep(delay)
-
-playsound.playsound('880Hz_tone.mp3')
+def gen_tone(rand_delay=True, my_delay=-1):
+    # If rand_delay is False, there will be no time delay before playing the beep
+    # my_delay: float - used for setting a consistent delay time (in seconds)
+    if rand_delay:
+        delay = randint(2, 9) + 3
+        print(delay)
+        sleep(delay)
+    if my_delay != -1:
+        delay = my_delay
+        print(delay)
+        sleep(delay)
+    if my_delay != -1 and not rand_delay:
+        print("If you wanted a random delay, please don't set my_delay.")
+        print("If you wanted to set the delay yourself, please set rand_delay to False")
+    playsound.playsound('880Hz_tone.mp3')
